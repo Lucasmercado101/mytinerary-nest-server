1e0f2a17f9d3d5672c47326675b65670c67a4c05
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-10 12:56:23 -0300

    added typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CitiesModule],
+  imports: [
+    CitiesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      // TODO: turn off in prod
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cities/entities/city.entity.ts
+++ src/cities/entities/city.entity.ts
@@ -1,1 +1,13 @@
-export class City {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  country: string;
+}
