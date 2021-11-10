98fa6837ba826328d529a1b934c2adcada936a05
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-10 14:54:36 -0300

    added ParseIntPipe to cities get/:id

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,3 +1,4 @@
+import { ParseIntPipe } from '@nestjs/common';
 import {
   Controller,
   Get,
@@ -26,8 +27,8 @@ export class CitiesController {
   // }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.citiesService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.citiesService.findOne(id);
   }
 
   // @Patch(':id')
