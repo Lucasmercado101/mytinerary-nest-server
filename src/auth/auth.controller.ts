20fdbcde00e3326692921e46d4abccfc593afefc
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 16:05:59 -0300

    added login fn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { createUserDTO } from './dto/create-user.dto';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,12 @@ export class AuthController {
   @Post('register')
   async register(@Body() newUser: createUserDTO) {
     return this.authService.register(newUser);
+  }
+
+  @Post('login')
+  async login(@Body() user: createUserDTO, @Res() response: Response) {
+    const sessionId = await this.authService.login(user);
+    response.cookie('sid', sessionId);
+    return response.status(204);
   }
 }
