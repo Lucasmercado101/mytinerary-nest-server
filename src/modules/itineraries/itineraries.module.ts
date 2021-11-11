9440453cf8ef323e5f1cb58be870cb16fcd23042
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 17:59:47 -0300

    added TypeOrmModule

--- src/modules/itineraries/itineraries.module.ts
+++ src/modules/itineraries/itineraries.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ItinerariesService } from './itineraries.service';
 import { ItinerariesController } from './itineraries.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Itinerary } from './entities/itinerary.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Itinerary])],
+  providers: [ItinerariesService],
   controllers: [ItinerariesController],
-  providers: [ItinerariesService]
 })
 export class ItinerariesModule {}
