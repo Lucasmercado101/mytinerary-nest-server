f4425a2f1045d55945ccd828aeca522041ffbfe3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 18:52:47 -0300

    added itinerariesComments

--- src/modules/itineraries/itineraries.module.ts
+++ src/modules/itineraries/itineraries.module.ts
@@ -3,11 +3,14 @@ import { ItinerariesService } from './itineraries.service';
 import { ItinerariesController } from './itineraries.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Itinerary } from './entities/itinerary.entity';
+import { ItinerariesCommentsController } from './itineraries-comments.controller';
+import { ItineraryComment } from './entities/comment.entity';
+import { ItinerariesCommentsService } from './itineraries-comments.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Itinerary])],
-  providers: [ItinerariesService],
-  controllers: [ItinerariesController],
+  imports: [TypeOrmModule.forFeature([Itinerary, ItineraryComment])],
+  providers: [ItinerariesService, ItinerariesCommentsService],
+  controllers: [ItinerariesController, ItinerariesCommentsController],
   exports: [ItinerariesService],
 })
 export class ItinerariesModule {}
