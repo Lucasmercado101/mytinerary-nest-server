ea5605afd79798cef40821fcfbddf43dfadef4d2
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 16:54:22 -0300

    added Sessions and Users modules

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -3,9 +3,11 @@ import { CitiesService } from './cities.service';
 import { CitiesController } from './cities.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { City } from './entities/city.entity';
+import { SessionsModule } from 'src/sessions/sessions.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([City])],
+  imports: [TypeOrmModule.forFeature([City]), SessionsModule, UsersModule],
   controllers: [CitiesController],
   providers: [CitiesService],
 })
