7fd81aa0ce7552cbc9642252d3a3ba159f233ee3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 19:11:19 -0300

    added createOne

--- src/modules/itineraries/itineraries-comments.service.ts
+++ src/modules/itineraries/itineraries-comments.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
+import { CreateItineraryCommentDto } from './dto/create-comment.dto';
 import { UpdateItineraryDto } from './dto/update-itinerary.dto';
+import { ItineraryComment } from './entities/comment.entity';
 import { Itinerary } from './entities/itinerary.entity';
+import { ItinerariesService } from './itineraries.service';
 
 @Injectable()
 export class ItinerariesCommentsService {
   constructor(
-    @InjectRepository(Itinerary)
-    private itinerariesRepository: Repository<Itinerary>,
+    @InjectRepository(ItineraryComment)
+    private itinerariesCommentsRepository: Repository<ItineraryComment>,
+    private readonly itinerariesService: ItinerariesService,
+    private readonly usersService: UsersService,
   ) {}
+
+  async createOne(newCommentDto: CreateItineraryCommentDto) {
+    const user = await this.usersService.findOneById(newCommentDto.author_id);
+    return this.itinerariesCommentsRepository.save({
+      comment: newCommentDto.comment,
+      author: user,
+    });
+  }
 }
