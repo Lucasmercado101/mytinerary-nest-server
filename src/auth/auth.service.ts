bb70c013d84708dd8e2757e2998addd6728f61a4
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 16:05:07 -0300

    added login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { createUserDTO } from './dto/create-user.dto';
+import { SessionsService } from 'src/sessions/sessions.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly sessionsService: SessionsService,
+  ) {}
 
   register(newUserData: createUserDTO) {
     return this.usersService.createOne(newUserData);
+  }
+
+  async login(userData: createUserDTO) {
+    const user = await this.usersService.findOneByUsername(userData.username);
+    const { session_id } = await this.sessionsService.createSession(user.id);
+    return session_id;
   }
 }
