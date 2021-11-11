4f1ac196052e7c8ca784b021c0c1647c3e1413f1
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 19:10:41 -0300

    fixed

--- src/decorators/plainBody.decorator.ts
+++ src/decorators/plainBody.decorator.ts
@@ -1,18 +1,22 @@
 import * as rawbody from 'raw-body';
 import {
   createParamDecorator,
+  ExecutionContext,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
 
-export const PlainBody = createParamDecorator(async (data, req) => {
-  if (req.readable) {
-    return (await rawbody(req)).toString().trim();
-  }
-  throw new HttpException(
-    'Body aint text/plain',
-    HttpStatus.INTERNAL_SERVER_ERROR,
-  );
-});
+export const PlainBody = createParamDecorator(
+  async (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    if (req.readable) {
+      return (await rawbody(req)).toString().trim();
+    }
+    throw new HttpException(
+      'Body aint text/plain',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  },
+);
 
 // https://stackoverflow.com/a/54096331
