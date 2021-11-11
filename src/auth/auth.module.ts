efaad97e7167c8e8da8cd86fc5ef81326213ee8d
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 16:05:52 -0300

    added sessionsModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { SessionsModule } from 'src/sessions/sessions.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, SessionsModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
