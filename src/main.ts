85e2c46df07aaf7c47e89134dbf4ba2a80fe2259
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 16:05:41 -0300

    added cookieparser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser('secret'));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
