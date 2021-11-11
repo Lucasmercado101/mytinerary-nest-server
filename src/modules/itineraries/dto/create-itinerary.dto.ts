79baaa4e550cf52070da431045ca3a3ef9dd484d
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 17:54:52 -0300

    added validation

--- src/modules/itineraries/dto/create-itinerary.dto.ts
+++ src/modules/itineraries/dto/create-itinerary.dto.ts
@@ -1,1 +1,33 @@
-export class CreateItineraryDto {}
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateItineraryDto {
+  @IsString()
+  title: string;
+
+  @IsNumber()
+  authorId: number;
+
+  @IsNumber()
+  duration: number;
+
+  @IsNumber()
+  price: number;
+
+  @IsString({ each: true })
+  @ArrayMaxSize(3)
+  @ArrayMinSize(1)
+  tags: string[];
+
+  @IsString({ each: true })
+  @ArrayMaxSize(50)
+  @ArrayMinSize(1)
+  activities: string[];
+
+  @IsNumber()
+  cityId: number;
+}
