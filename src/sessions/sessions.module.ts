3dbee3fc14c04f74cef864ce63fb01deb102794e
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-11 15:43:27 -0300

    deleted session controller import line

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
-import { SessionsController } from './sessions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Session } from './entities/session.entity';
 
