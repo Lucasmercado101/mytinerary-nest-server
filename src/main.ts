16eef3ff82a21ca588dd3722b2629aa60b39243e
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-15 14:14:07 -0300

    WIP: CORS

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: (_, callback) => callback(null, { origin: true }),
+      credentials: true,
+    }),
+  );
   app.use(cookieParser('secret'));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(8001);
 }
 bootstrap();
