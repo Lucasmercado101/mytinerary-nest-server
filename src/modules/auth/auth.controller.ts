1c388ec5a12f76c9e914fe210e5197f59cbc4ae7
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-15 14:19:40 -0300

    login returns user dto

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { createUserDTO } from './dto/create-user.dto';
 import { Request, Response } from 'express';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post('register')
   async register(@Body() newUser: createUserDTO) {
@@ -16,7 +20,9 @@ export class AuthController {
   async login(@Body() user: createUserDTO, @Res() response: Response) {
     const sessionId = await this.authService.login(user);
     response.cookie('sid', sessionId);
-    return response.sendStatus(204);
+    return response.json(
+      await this.usersService.findOneByUsername(user.username),
+    );
   }
 
   @Get('logout')
