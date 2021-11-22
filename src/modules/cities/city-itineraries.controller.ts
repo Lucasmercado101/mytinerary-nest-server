3553e7400f7417cfb4a503d12822d501a95be29b
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-22 14:35:52 -0300

    removed unused imports

--- src/modules/cities/city-itineraries.controller.ts
+++ src/modules/cities/city-itineraries.controller.ts
@@ -1,10 +1,9 @@
-import { Delete, ParseIntPipe, Patch, UseGuards } from '@nestjs/common';
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { User } from 'src/decorators/user.decorator';
 import { CityExistsGuard } from 'src/guards/cityExists.guard';
 import { IsLoggedInGuard } from 'src/guards/isLoggedIn.guard';
 import { CreateItineraryDto } from '../itineraries/dto/create-itinerary.dto';
-import { UpdateItineraryDto } from '../itineraries/dto/update-itinerary.dto';
 import { ItinerariesService } from '../itineraries/itineraries.service';
 import { User as UserEntity } from '../users/entity/user.entity';
 
