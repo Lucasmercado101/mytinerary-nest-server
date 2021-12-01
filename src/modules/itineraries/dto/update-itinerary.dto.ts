20db1b7960792b3abdcbad364cbdcf79f202285f
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-01 18:06:08 -0300

    fixed

--- src/modules/itineraries/dto/update-itinerary.dto.ts
+++ src/modules/itineraries/dto/update-itinerary.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateItineraryDto } from './create-itinerary.dto';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
-export class UpdateItineraryDto extends PartialType(CreateItineraryDto) {}
+export class UpdateItineraryDto {
+  @IsString()
+  title: string;
+
+  @IsNumber()
+  time: number;
+
+  @IsNumber()
+  price: number;
+
+  @IsString({ each: true })
+  @ArrayMaxSize(3)
+  @ArrayMinSize(1)
+  hashtags: string[];
+
+  @IsString({ each: true })
+  @ArrayMaxSize(50)
+  @ArrayMinSize(1)
+  activities: string[];
+}
