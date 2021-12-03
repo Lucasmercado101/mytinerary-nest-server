625f8971df4ef204b9a8c064ee35d24568796122
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-03 14:22:51 -0300

    disabled json parser expect for /auth/refresh

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
+import { json } from 'body-parser';
+
+const jsonParseMiddleware = json();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
+
   app.use(
     cors({
       origin: (_, callback) => callback(null, { origin: true }),
@@ -15,6 +21,16 @@ async function bootstrap() {
   );
   app.use(morgan('dev'));
   app.use(cookieParser('secret'));
+
+  app.use((req: any, res: any, next: any) => {
+    // do not parse json bodies if we are hitting refresh
+    if (req.path.indexOf('/auth/refresh') === 0) {
+      next();
+    } else {
+      jsonParseMiddleware(req, res, next);
+    }
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8001);
 }
